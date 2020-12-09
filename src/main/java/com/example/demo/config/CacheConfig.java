232a3d2fd7bfcb3dabf773529fb3fcecd81e231f
sieunkim <sieunkr@gmail.com>
2020-12-10 00:14:16 +0900

    cachemanager 커스텀 설정

--- src/main/java/com/example/demo/config/CacheConfig.java
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -1,9 +1,60 @@
 package com.example.demo.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @Configuration
-public class CacheConfig {
+@RequiredArgsConstructor
+public class CacheConfig extends CachingConfigurerSupport {
 
-    //TODO: redis 컨피그 설정
+    private final CacheConfigurationProperties cacheConfigurationProperties;
+
+    private static RedisCacheConfiguration createCacheConfiguration(long timeoutInSeconds) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(timeoutInSeconds));
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(cacheConfigurationProperties.getHost());
+        redisStandaloneConfiguration.setPort(cacheConfigurationProperties.getPort());
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        for (Map.Entry<String, Long> cacheNameAndTimeout : cacheConfigurationProperties.getCacheExpirations().entrySet()) {
+            cacheConfigurations.put(cacheNameAndTimeout.getKey(), createCacheConfiguration(cacheNameAndTimeout.getValue()));
+        }
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(createCacheConfiguration(cacheConfigurationProperties.getTimeoutSeconds()))
+                .withInitialCacheConfigurations(cacheConfigurations).build();
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
 }
